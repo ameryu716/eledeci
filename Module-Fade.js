637d04bd1f9cc0e9365bda86c20c0aee47079515
ameryu716 <72952772+ameryu716@users.noreply.github.com>
2020-11-13 13:24:33 +0900

    フェードモジュールを複数要素の同時指定に対応させました。

--- Module-Fade.js
+++ Module-Fade.js
@@ -4,3 +4,16 @@ export const Fadeout = function(elemen){
 export const Fadein = function(elemen){
     elemen.style.display = "block";
 }
+
+export const MulchFadein = (...palam)=>{
+    for(let i=0;i<palam.length;i++){
+        palam[i].style.display = "block";
+    }
+    console.log(palam);
+}
+export const MulchFadeout = (...palam)=>{
+    for(let i=0;i<palam.length;i++){
+        palam[i].style.display = "none";
+    }
+    console.log(palam);
+}
