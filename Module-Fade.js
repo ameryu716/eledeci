f8defc061eb2916a15a84beca907ae58662eebab
ameryu716 <72952772+ameryu716@users.noreply.github.com>
2020-11-15 00:07:21 +0900

    DelayFadeinメソッドを追加し、CSSのopacityプロパティによる遅延効果とノードの複数同時指定にも対応しました。

--- Module-Fade.js
+++ Module-Fade.js
@@ -17,3 +17,35 @@ export const MulchFadeout = (...palam)=>{
     }
     console.log(palam);
 }
+
+export const DelayFadein = (delay,...node)=>{
+    if(delay<10){
+        for(let i=0;i<node.length;i++){
+            node[i].style.display = "block";
+            node[i].style.transition = 0;
+            node[i].style.opacity = "0";
+            node[i].style.transition = delay+"s";
+            setTimeout(()=>{
+                node[i].style.opacity = "1";
+            },100)
+        }
+    }else{
+        console.error("フェード秒数が長すぎます");
+    }
+}//※フェード秒数は10秒以内です
+
+export const DelayFadeout = (delay,...node)=>{
+    if(delay<10){
+        for(let i=0;i<node.length;i++){
+            node[i].style.transition = 0;
+            node[i].style.opacity = "1";
+            node[i].style.transition = delay+"s";
+            node[i].style.opacity = "0";
+            setTimeout(()=>{
+                node[i].style.display = "none";
+            },delay*900)
+        }
+    }else{
+        console.error("フェード秒数が長すぎます");
+    }
+}//※フェード秒数は10秒以内です
